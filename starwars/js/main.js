3fcdb8b1e276404f4b2ff11ec7e51a332b5b098e
Alex Williams <Alex Williams>
2017-04-22 17:39:35 -0700

    added more css stylings

--- starwars/js/main.js
+++ starwars/js/main.js
@@ -1,4 +1,5 @@
  function getStarwarsPeople() {
+        
         var personsList = document.getElementById('js-names');
         var personsListContent = '';
         var people;
@@ -14,14 +15,13 @@
 
             for(var i = 0; i < people.length; i++){
               console.log('for loop is working');
-              personsListContent += '<h2>' + people[i].name +'</h2>';
+              personsListContent += '<h2 class="peopleName">' + people[i].name +'</h2>';
               personsListContent += '<h3>' + people[i].skin_color+'</h3>';
               personsListContent += '<h3>' + people[i].gender +'</h3>';
               personsListContent += '<h3>' + people[i].hair_color +'</h3>';
             }
 
               personList.innerHTML = personsListContent;
-
 
           },
           error: function(err) {
